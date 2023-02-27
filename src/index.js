bcc2f02b2fc5fd3db4efc50d2e00eb7132d3f25b
Lucas Silva <lucassilva.uern@gmail.com>
2023-02-27 16:22:44 -0300

    feat: inicializando projeto express e inst eslint

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3333;
+
+app.get('/', (request, response) => {
+  response.send('Teste');
+});
+
+app.listen(port, () =>
+  console.log(`🤞 Server started at http://localhost:${port}`)
+);
