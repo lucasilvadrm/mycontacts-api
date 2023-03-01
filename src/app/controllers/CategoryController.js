e0ce31e0f3213155a928464834337f28fdb4d7b7
Lucas Silva <lucassilva.uern@gmail.com>
2023-03-01 10:46:51 -0300

    feat: finalizando rotas, contoller e repository categories

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -6,7 +6,17 @@ class CategoryController {
       const categories = await CategoriesRepository.findAll();
       response.status(200).json(categories);
     } catch (error) {
-      response.status(400).json(error);
+      response.status(400).json({ error });
+    }
+  }
+
+  async show(request, response) {
+    try {
+      const { id } = request.params;
+      const category = await CategoriesRepository.findById(id);
+      response.status(200).json(category);
+    } catch (error) {
+      response.status(400).json({ error });
     }
   }
 
@@ -20,6 +30,32 @@ class CategoryController {
     const category = await CategoriesRepository.create({ name });
 
     response.json(category);
+  }
+
+  async update(request, response) {
+    const { id } = request.params;
+    const { name } = request.body;
+    try {
+      const categoryExists = await CategoriesRepository.findById(id);
+      if (!categoryExists) {
+        return response.status(404).json({ error: 'Category not found' });
+      }
+
+      if (!name) {
+        return response.status(400).json({ error: 'Name is required' });
+      }
+
+      const updatedCategory = await CategoriesRepository.update(id, { name });
+      response.status(200).json(updatedCategory);
+    } catch (error) {
+      response.status(400).json({ error });
+    }
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+    await CategoriesRepository.delete(id);
+    response.sendStatus(204);
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -17,6 +17,9 @@ router.put('/contacts/:id', ContactController.update);
 
 // categories
 router.get('/categories', CategoryController.index);
+router.get('/categories/:id', CategoryController.show);
 router.post('/categories', CategoryController.store);
+router.put('/categories/:id', CategoryController.update);
+router.delete('/categories/:id', CategoryController.delete);
 
 module.exports = router;
