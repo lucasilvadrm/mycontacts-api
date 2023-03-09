8cc380810a3681c0afa6ffd8cc26f6321ebcd868
Lucas Silva <lucassilva.uern@gmail.com>
2023-03-09 19:04:30 -0300

    feat: add config no cors

--- src/routes.js
+++ src/routes.js
@@ -6,10 +6,7 @@ const ContactController = require('./app/controllers/ContactController');
 const router = Router();
 
 // contacts
-router.get('/contacts', (request, response, next) => {
-  request.appId = 'MeuAppID';
-  next();
-}, ContactController.index);
+router.get('/contacts', ContactController.index);
 router.get('/contacts/:id', ContactController.show);
 router.delete('/contacts/:id', ContactController.delete);
 router.post('/contacts', ContactController.store);
